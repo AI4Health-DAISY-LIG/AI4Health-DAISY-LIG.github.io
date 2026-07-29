667d6297a2881c8b1bd7c0c5bed6c243c107df6d
Sandrine Muller <153299800+sandrine-muller@users.noreply.github.com>
2026-07-29 22:14:33 +0200

    site description

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import { Link } from '@/lib/transition'
 export default function Home() {
   return (
     <section className="flex min-h-[86.5vh] flex-col items-center justify-center px-2 py-8 text-center">
-      <h1 className="mb-4 text-4xl font-bold sm:text-7xl">Documents</h1>
+      <h1 className="mb-4 text-4xl font-bold sm:text-7xl">AI4Health</h1>
       <p className="mb-8 max-w-150 text-foreground sm:text-base">
-        A simple open-source product documentation platform. That&apos;s simple to use and easy to
-        customize.
+        Advancing Systems Biology through end-to-end Artificial Intelligence to better suit real health data challenges. 
+        @DAISY team, LIGLAB, Grenoble, France
       </p>
 
       <div className="flex items-center gap-5">
