095c0639bfc7fe4314d913fb4c80770651d25b49
Sandrine Muller <153299800+sandrine-muller@users.noreply.github.com>
2026-07-29 22:17:16 +0200

    Update navigation.ts
    setting top navigation bar

--- settings/navigation.ts
+++ settings/navigation.ts
@@ -2,16 +2,36 @@ import { PageRoutes } from '@/lib/pageroutes'
 
 export const Navigations = [
   {
-    title: 'Docs',
+    title: 'Main',
     href: `/docs${PageRoutes[0].href}`,
   },
   {
-    title: 'Rubix',
-    href: 'https://rubixstudios.com.au',
-    external: true,
+    title: 'Projects',
+    href: `/docs${PageRoutes[0].href}`,
   },
+  {
+    title: 'Our group',
+    href: `/docs${PageRoutes[0].href}`,
+  },
+  {
+    title: 'Openings',
+    href: `/docs${PageRoutes[0].href}`,
+  },
+  {
+    title: 'Meetings',
+    href: `/docs${PageRoutes[0].href}`,
+  },
+  {
+    title: 'Find us',
+    href: `/docs${PageRoutes[0].href}`,
+  },
+  // {
+  //   title: 'Rubix',
+  //   href: 'https://rubixstudios.com.au',
+  //   external: true,
+  // },
 ]
 
 export const GitHubLink = {
-  href: 'https://github.com/rubixvi/rubix-documents',
+  href: 'https://github.com/AI4Health-DAISY-LIG',
 }
