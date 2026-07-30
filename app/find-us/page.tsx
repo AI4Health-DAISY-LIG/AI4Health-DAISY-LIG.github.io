8214471a2d773790f6df03ed0923082085148622
Sandrine Muller <153299800+sandrine-muller@users.noreply.github.com>
2026-07-30 18:51:37 +0200

    fix: supprimer les apostrophes dans l'adresse

--- app/find-us/page.tsx
+++ app/find-us/page.tsx
@@ -15,10 +15,10 @@ export default function Home() {
         If so, we would love to hear from you! We are always looking for people who want to collaborate on projects that will help us improve our research and make it more accessible to others.</p>
       <h2 className="mb-4 text-4xl font-bold sm:text-7xl">Visit the LIGLAB</h2>
       <p>
-        LIG - Laboratoire d’Informatique de Grenoble 
+        LIG - Laboratoire d Informatique de Grenoble 
         sandrine.muller [at] univ-grenoble-alpes.fr 
         Office 303 (third floor) 
-        700 Avenue Centrale 38401 Saint-Martin-d’Hères, France
+        700 Avenue Centrale 38401 Saint-Martin-d Hères, France
       </p>
       <div className="flex items-center gap-5">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2811.8172375578383!2d5.764789711875912!3d45.190795751518415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478af50837597d0d%3A0x7089f5efffec5353!2sGrenoble%20Computer%20Science%20Laboratory%20-%20LIGLAB!5e0!3m2!1sen!2sfr!4v1745851827454!5m2!1sen!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
