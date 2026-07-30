c7798144a310337077496ca623aef3306b4ad1d4
Sandrine Muller <153299800+sandrine-muller@users.noreply.github.com>
2026-07-30 13:42:26 +0200

    Creation of cards collections for our group tab

---
+++ app/our-group/page.tsx
@@ -0,0 +1,58 @@
+import { Card } from '@/components/markdown/card'
+import { getProjects } from '@/lib/markdown'
+
+export default async function ProjectsPage() {
+  const projects = await getProjects()
+
+  return (
+    <section className="py-12 px-5 sm:px-8">
+      <h1 className="text-4xl font-bold mb-10 text-center">Advancing systems biology through end-to-end Artificial Intelligence</h1>
+      <p>Our group develops sophisticated learning architectures to bridge the gap between raw biological data and mechanistic understanding. We specialize in building robust, end-to-end frameworks—from Bayesian inference to reinforcement learning—designed to decode the multi-scale complexity of biological systems.
+
+      * *Data Representation* : Knowledge graphs, ontologies, and relational databases.
+      * *Advanced Modeling* : Structural learning, statistical modeling, and AI-assisted discovery.
+      * *Engineering Excellence* : Optimized APIs and high-performance computing pipelines.
+      
+      *Our scientific engagement* : Open Science • Reproducibility • Explainable AI • Co-design
+      </p>
+      <h2 className="text-4xl font-bold mb-10 text-center">Members</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects.map((project) => (
+          <Card
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            href={project.href}
+            image={project.image}
+          />
+        ))}
+      </div>
+      
+      <h2 className="text-4xl font-bold mb-10 text-center">Students</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects.map((project) => (
+          <Card
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            href={project.href}
+            image={project.image}
+          />
+        ))}
+      </div>
+      
+      <h2 className="text-4xl font-bold mb-10 text-center">Alumni</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects.map((project) => (
+          <Card
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            href={project.href}
+            image={project.image}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
